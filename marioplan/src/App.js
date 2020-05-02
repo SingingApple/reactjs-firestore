cf74a8bea48a0051ad49e1e2a929bdb2adc0b585
Anurag Srivastava <anurag.as.srivastava@gmail.com>
2020-05-02 23:19:40 +0530

    Nav v1

--- marioplan/src/App.js
+++ marioplan/src/App.js
@@ -3,6 +3,9 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Dashboard from "./components/dashboard/Dashboard";
 import ProjectDetails from "./components/project/ProjectDetails";
+import SignIn from "./components/auth/SignIn";
+import SignUp from "./components/auth/SignUp";
+import CreateProject from "./components/project/CreateProject";
 class App extends Component {
   render() {
     return (
@@ -12,6 +15,9 @@ class App extends Component {
           <Switch>
             <Route path="/" exact component={Dashboard}></Route>
             <Route path="/projects/:id" component={ProjectDetails}></Route>
+            <Route path="/signin" component={SignIn}></Route>
+            <Route path="/signup" component={SignUp}></Route>
+            <Route path="/create" component={CreateProject}></Route>
           </Switch>
         </div>
       </BrowserRouter>
--- marioplan/src/components/layout/SignedOutLinks.jsx
+++ marioplan/src/components/layout/SignedOutLinks.jsx
@@ -4,10 +4,10 @@ const SignedOutLinks = () => {
   return (
     <ul className="right">
       <li>
-        <NavLink to="/">Sign In</NavLink>
+        <NavLink to="/signin">Sign In</NavLink>
       </li>
       <li>
-        <NavLink to="/">Sign Up</NavLink>
+        <NavLink to="/signup">Sign Up</NavLink>
       </li>
     </ul>
   );
