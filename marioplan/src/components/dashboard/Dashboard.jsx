777b9cf453c2bce453ca365edcafcd3c8117620a
Anurag Srivastava <anurag.as.srivastava@gmail.com>
2020-05-06 15:30:21 +0530

    Starting Thunk

--- marioplan/src/components/dashboard/Dashboard.jsx
+++ marioplan/src/components/dashboard/Dashboard.jsx
@@ -1,14 +1,16 @@
 import React, { Component } from "react";
 import Notifications from "./Notifications";
 import ProjectList from "../project/ProjectList";
+import { connect } from "react-redux";
 class Dashboard extends Component {
   render() {
+    const { projects } = this.props;
     return (
       <div>
         <div className="container dashboard">
           <div className="row">
             <div className="col s12 m6">
-              <ProjectList></ProjectList>
+              <ProjectList projects={projects}></ProjectList>
             </div>
             <div className="col s12 m5 offset-m1">
               <Notifications></Notifications>
@@ -19,5 +21,9 @@ class Dashboard extends Component {
     );
   }
 }
-
-export default Dashboard;
+const mapStateToProps = (state) => {
+  return {
+    projects: state.project.projects,
+  };
+};
+export default connect(mapStateToProps)(Dashboard);
--- marioplan/src/components/project/ProjectSummary.jsx
+++ marioplan/src/components/project/ProjectSummary.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { Links, Route } from "react-router-dom";
-const ProjectSummary = () => {
+const ProjectSummary = ({ project }) => {
   return (
     <div className="card z-depth-0 project-summary">
       <div className="card-content grey-text text-darken-3">
-        <span className="card-title">Project Title</span>
-        <p>Posted by Apple</p>
+        <span className="card-title">{project.title}</span>
+        <p>{project.content}</p>
         <p className="grey-text">20 November 2020</p>
       </div>
     </div>
